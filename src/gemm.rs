96926227086c024391a7015210ddfe60232d8491
sebastian <sebastian@datavant.com>
2024-11-26 15:26:36 -0700

    perf(gemm): seems like we're regressing here

--- src/gemm.rs
+++ src/gemm.rs
@@ -3,6 +3,8 @@ use std::ops::{Add, AddAssign, Mul};
 
 use crate::Matrix;
 
+const BLOCK_SIZE: usize = 64;
+
 pub fn matmul<T>(lhs: &Matrix<T>, rhs: &Matrix<T>) -> Matrix<T>
 where
     T: Add<Output = T> + AddAssign + Mul<Output = T> + Copy + Default,
@@ -15,11 +17,22 @@ where
         );
     }
 
+    // Transpose rhs for cache locality
+    let rhs_t = rhs.transpose();
+
     let mut result = Matrix::new(lhs.n_rows, rhs.n_cols);
-    for k in 0..rhs.n_rows {
-        for i in 0..lhs.n_rows {
-            for j in 0..rhs.n_cols {
-                result[(i, j)] += lhs[(i, k)] * rhs[(k, j)];
+    for bi in (0..lhs.n_rows).step_by(BLOCK_SIZE) {
+        for bj in (0..rhs.n_cols).step_by(BLOCK_SIZE) {
+            for bk in (0..rhs.n_rows).step_by(BLOCK_SIZE) {
+                for i in bi..bi + BLOCK_SIZE {
+                    for j in bj..bj + BLOCK_SIZE {
+                        let mut acc = T::default();
+                        for k in bk..bk + BLOCK_SIZE {
+                            acc += lhs[(i, k)] * rhs_t[(j, k)];
+                        }
+                        result[(i, j)] += acc;
+                    }
+                }
             }
         }
     }
