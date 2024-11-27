9885ea500863e0b100995bd2e406b39e73b45ffb
sebastian <sebastian@datavant.com>
2024-11-27 05:25:51 -0700

    perf(gemm): trying to transpose the matrix

--- benches/matmul.rs
+++ benches/matmul.rs
@@ -9,6 +9,17 @@ fn main() {
 }
 
 #[divan::bench(args = [128, 512, 1024])]
+fn transpose_bench(bencher: divan::Bencher, n: usize) {
+    let gen: fn() -> f32 = || rand::thread_rng().gen();
+
+    let a = Matrix::from_gen(n, n, gen);
+
+    bencher.bench(|| {
+        a.transpose();
+    })
+}
+
+#[divan::bench(args = [128, 512, 1024])]
 fn matmul_bench(bencher: divan::Bencher, n: usize) {
     let gen: fn() -> f32 = || rand::thread_rng().gen();
 
