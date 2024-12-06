220cc3271ce677b6101040c0bf462afc014a478c
Sebastian Claici <sebastianclaici@gmail.com>
2024-12-05 17:46:06 -0700

    test(chol): test correctness

--- src/linalg/chol.rs
+++ src/linalg/chol.rs
@@ -1,6 +1,5 @@
 use crate::core::error::NotPositiveDefiniteError;
-use crate::core::matrix::matrix;
-use crate::core::matrix::Matrix;
+use crate::core::matrix::*;
 
 #[derive(Debug)]
 pub struct Cholesky<T> {
@@ -55,6 +54,7 @@ impl_cholesky!(f64);
 #[cfg(test)]
 mod test {
     use super::*;
+    use approx::assert_relative_eq;
 
     #[test]
     fn test_cholesky() {
@@ -64,6 +64,15 @@ mod test {
         assert!(chol.is_ok());
 
         let chol = chol.unwrap();
+        assert_relative_eq!(chol.chol_l[(0, 0)], 2.0);
+        assert_relative_eq!(chol.chol_l[(1, 0)], 6.0);
+        assert_relative_eq!(chol.chol_l[(2, 0)], -8.0);
+        assert_relative_eq!(chol.chol_l[(0, 1)], 0.0);
+        assert_relative_eq!(chol.chol_l[(1, 1)], 1.0);
+        assert_relative_eq!(chol.chol_l[(2, 1)], 5.0);
+        assert_relative_eq!(chol.chol_l[(0, 2)], 0.0);
+        assert_relative_eq!(chol.chol_l[(1, 2)], 0.0);
+        assert_relative_eq!(chol.chol_l[(2, 2)], 3.0);
     }
 
     #[test]
